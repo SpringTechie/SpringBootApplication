d3a2bd7b6826bf98f537e6a145e2fbf0a5f5334e
SpringTechie <153965307+SpringTechie@users.noreply.github.com>
2024-02-16 22:19:14 +0530

    Update UserRepository.java

--- src/main/java/com/spring/aws/repo/UserRepository.java
+++ src/main/java/com/spring/aws/repo/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.spring.aws.entity.User;
 
 public interface UserRepository  extends JpaRepository<User, Integer>{
-    // hello raja
+    // hello raja ggg
 }
